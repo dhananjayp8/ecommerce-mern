57b2d712b84a2d9f5e39e5c2df95f2f40ebbdf73
Dhananjay Puranik <dhananjaypuranik8@gmail.com>
2024-04-18 22:13:25 +0530

    added key

--- server/model/admin/adminModel.js
+++ server/model/admin/adminModel.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const validator = require("validator");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-const SECRET_KEY = "dhananjaypuranik";
+const SECRET_KEY = process.env.SECRET_KEY;
 const adminSchema = new mongoose.Schema({
   name: {
     type: String,
